a0392753f6f41c2c5bb1c72378f50e374ccf2463
404RX <60455179+404RX@users.noreply.github.com>
2025-06-07 08:43:45 -0400

    Updating api bot

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "../themes/toha/layouts/**/*.html",
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "../themes/toha/static/js/**/*.js"
   ],
   theme: {
     extend: {
       colors: {
         primary: {
           DEFAULT: '#007bff',
-          dark: '#0056b3',
-        },
-      },
-    },
+          dark: '#0056b3'
+        }
+      }
+    }
   },
-  plugins: [],
   // Ensure Tailwind's base styles don't conflict with existing styles
   corePlugins: {
-    preflight: false,
-  },
+    preflight: false
+  }
 } 
